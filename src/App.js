e0b1039eb8780013924b95658a835d2aa2162d08
Joel Chua <joel.chua.mingyi@gmail.com>
2023-03-07 22:05:08 +0800

    add image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import joel from './joel.jpg';
 
 const App = () => {
   return (
        <div className="App">
-        <img src="https://uploads.codesandbox.io/uploads/user/97c18696-1b85-4851-a39c-5b83b339ffd7/sdDG-generic.png" alt="Generic"/>
+        <img src={joel} alt="Generic"/>
     <h1>Joel</h1>
     <h2>Software Engineer</h2>
     <div className="_src_component_Section_module_card">
